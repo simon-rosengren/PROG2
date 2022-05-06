d40e11d68d1cd55ac6bc02d3b42d33a6dd5c6071
idaamneryd <82778859+idaamneryd@users.noreply.github.com>
2022-05-06 14:26:07 +0200

    fredag

--- PathFinder.java
+++ PathFinder.java
@@ -1,41 +1,69 @@
 import javafx.application.Application;
-import javafx.geometry.Orientation;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class PathFinder extends Application{
+
+    private Stage primaryStage;
+    private BorderPane root = new BorderPane();
+    private VBox file = new VBox();
+    private Pane center = new Pane();
+    private ImageView imageView = new ImageView();
+
     @Override
     public void start (Stage primaryStage){
-        BorderPane root = new BorderPane();
+        this.primaryStage = primaryStage;
 
-        Label labelTitle = new Label("File");
+        setFileBar();
+        setFlowPane();
 
-        FlowPane flowPaneCenter = new FlowPane(Orientation.HORIZONTAL);
+        center = new Pane();
+        center.getChildren().add(imageView);
+        root.setCenter(center);
 
+        Scene scene = new Scene(new VBox(file, root), 1200, 900);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void setFileBar(){
+        file = new VBox();
+        MenuBar menubar = new MenuBar();
+        file.getChildren().add(menubar);
+        Menu fileMenu = new Menu("File");
+        menubar.getMenus().add(fileMenu);
+        MenuItem newMap = new MenuItem("New Map");
+        MenuItem open = new MenuItem("Open");
+        MenuItem save = new MenuItem("Save");
+        MenuItem saveImage = new MenuItem("Save Image");
+        MenuItem exit = new MenuItem("Exit");
+
+        fileMenu.getItems().addAll(newMap, open, save, saveImage, exit);
+    }
+
+    public void setFlowPane(){
+        VBox topVBox = new VBox();
         Button findPath = new Button("Find Path");
         Button showConnection = new Button("Show Connection");
         Button newPlace = new Button("New Place");
         Button newConnection = new Button("New Connection");
         Button changeConnection = new Button("Change Connection");
 
-        flowPaneCenter.getChildren().addAll(findPath, showConnection, newPlace, newConnection, changeConnection);
-
-        root.getChildren().add(labelTitle);
-        root.getChildren().add(flowPaneCenter);
-
-        /*
-        root.setTop(labelTitle);
-        root.setCenter(flowPaneCenter);
-        */
-
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        FlowPane top = new FlowPane();
+        topVBox.setSpacing(5);
+        top.setAlignment(Pos.CENTER);
+        top.setPadding(new Insets(5));
+        top.setHgap(5);
+        root.setTop(top);
+        top.getChildren().addAll(findPath, topVBox, showConnection, newPlace, newConnection, changeConnection);
     }
 
     public static void main(String [] args){
